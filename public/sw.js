efb2cca2415ffce8fe600aad2d18a9f45ed9bce8
Torrey Ramirez <torreyramirez.design@gmail.com>
2026-05-27 18:39:25 -0500

    icon

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'folja-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/manifest.json',
+  '/pwa-icon.svg',
+  '/pwa-icon-maskable.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
